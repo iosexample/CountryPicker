a3f3e055f4b61b5a12372366004c112caf8301db
Dong <donly@magicalboy.com>
2018-03-20 16:56:30 +0800

    add  CountryPickerViewController demo

--- Examples/CountryPickerDemo/ViewController.m
+++ Examples/CountryPickerDemo/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "CountryPickerViewController.h"
+
+@interface ViewController() <CountryPickerViewControllerDelegate>
+@end
 
 @implementation ViewController
 
@@ -18,6 +22,28 @@
     self.codeLabel.text = code;
     self.dailCodeLabel.text = dailCode;
     
+}
+
+- (IBAction)showCountryPicker:(id)sender {
+    CountryPickerViewController *picker = [[CountryPickerViewController alloc] init];
+    picker.delegate = self;
+    UINavigationController *navigationController = [[UINavigationController alloc]
+                                                    initWithRootViewController:picker];
+    UIBarButtonItem *cancelItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:(UIBarButtonSystemItemCancel) target:self action:@selector(cancel)];
+    [[picker navigationItem] setLeftBarButtonItem:cancelItem];
+    [self presentViewController:navigationController animated:YES completion:nil];
+}
+
+- (void)cancel {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)countryPickerViewController:(CountryPickerViewController *)countryPickerViewController didSelectCountryWithName:(NSString *)name code:(NSString *)code dialCode:(NSString *)dialCode
+{
+    self.nameLabel.text = name;
+    self.codeLabel.text = code;
+    self.dailCodeLabel.text = dialCode;
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
